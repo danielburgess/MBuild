323f4f4cf76e88411ba1865eb15703f5a6861d5c
DackR <dburgess@hedratek.com>
2016-02-16 00:11:41 -0500

    removed unfinished conversion of atlas

--- Helpers/ScriptTools/AtlasSharp/AtlasExtension.cs
+++
@@ -1,8 +0,0 @@
-﻿internal static partial class DefineConstants
-{
-	public const int TBL_OK = 0x00; // Success
-	public const int TBL_OPEN_ERROR = 0x01; // Cannot open the Table properly
-	public const int TBL_PARSE_ERROR = 0x02; // Cannot parse how the Table is typed
-	public const int NO_MATCHING_ENTRY = 0x10; // There was an entry that cannot be matched in the table
-	public const int SPACE = 0x20;
-}
